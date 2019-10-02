5fa5c56c04cb711057307bdc489bbe3447026b7a
Baldur van Lew <baldurvanlew@gmail.com>
2019-10-02 10:59:32 +0200

    Mover version functions to separate file for use during setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 PROJECT_DIR = os.path.dirname(__file__)
 sys.path.append(os.path.join(PROJECT_DIR, 'citools'))
 
-from citools import get_version
+from citools.version import get_version
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
